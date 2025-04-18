27a9b442761fae7b713759b3807c8aec4ba8dfe0
AmirHossein SHajari <ahshqhir@gmail.com>
2025-04-18 14:00:45 +0330

    added something

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: false,
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  title?: string;
+  get Title(): string {
+    return this.title ?? 'Default Title';
+  }
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.title = data['title'];
+    });
+  }
 }
