d9a7c1f328ba646ebcc1dd4a5c1dfd235e3ba008
matinshaah <shahnaziamirmatin@gmail.com>
2025-05-02 14:08:14 +0330

    load dotenv

--- manager/cmd/main.go
+++ manager/cmd/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
