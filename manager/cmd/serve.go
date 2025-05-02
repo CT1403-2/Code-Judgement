10099a304fbf637eeec76981f02ed60a9111b6fc
matinshaah <shahnaziamirmatin@gmail.com>
2025-05-02 23:50:41 +0330

    register server manager and server static files

--- manager/cmd/serve.go
+++ manager/cmd/serve.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/CT1403-2/Code-Judgement/proto"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 	"log"
 	"manger/internal/manager"
+	"net"
+	"net/http"
+	"os"
 )
 
 var serveCmd = &cobra.Command{
@@ -12,8 +17,10 @@ var serveCmd = &cobra.Command{
 	Short: "Starts the server",
 	Long:  `Starts the server and begins listening for requests`,
 	Run: func(cmd *cobra.Command, args []string) {
+		port, _ := cmd.Flags().GetString("port")
+
 		fmt.Println("Server started")
-		err := serve()
+		err := serve(port)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -24,14 +31,38 @@ func init() {
 	rootCmd.AddCommand(serveCmd)
 
 	// Add flags specific to this command
-	serveCmd.Flags().IntP("port", "p", 8080, "Port to run the server on")
+	serveCmd.Flags().StringP("port", "p", os.Getenv("SERVER_PORT"), "Port to run the server on")
 }
 
-func serve() error {
+func serve(port string) error {
+	fmt.Println()
+	go serveStaticFiles(port)
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		return fmt.Errorf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+
 	m, err := manager.NewManager()
 	if err != nil {
 		return err
 	}
-	err = m.Start()
+
+	proto.RegisterManagerServer(grpcServer, m)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		return fmt.Errorf("failed to serve: %v", err)
+	}
 	return err
+}
+
+func serveStaticFiles(port string) {
+	fs := http.FileServer(http.Dir("../../front/")) // Your static directory
+	http.Handle("/", fs)
+	addr := ":" + port
+	log.Printf("Serving static files on %v\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatalf("Static server failed: %v", err)
+	}
 }
